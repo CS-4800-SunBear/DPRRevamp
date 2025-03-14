55ffc7ea804f66f7b229cc1698af377bfe5509f8
Ethan <113479501+EthanSc-o7@users.noreply.github.com>
2025-03-13 19:16:26 -0700

    cleaned up test

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -3,17 +3,11 @@ import {jest} from 'jest';
 
 const getClasses = require('../fetch');
 
-test('courses properly appear', ()=>{
-  const array = []; 
-  expect(getClasses).not.toBe(array); 
-  //When no courses appear the array is empty, since the comparison fails
-  //the array must have classes
-})
 
 test('courses array is not empty', ()=>{
   const x = 0; 
   const size = getClasses.length
-  expect(size).not.toBe(0); 
+  expect(size).not.toBe(x); 
   //When no courses appear the array is empty, since the comparison fails
   //the array must have classes
 })
