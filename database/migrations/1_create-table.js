4b9eb42cc13e4b64790b248d9f7e7bee6defbac8
Ethan <113479501+EthanSc-o7@users.noreply.github.com>
2025-04-10 14:07:04 -0700

    updated database connection

--- database/migrations/1_create-table.js
+++ database/migrations/1_create-table.js
@@ -1,11 +1,13 @@
 const makeCourseTable = 
-`CREATE TABLE IF NOT EXISTS public.courses
+`CREATE TABLE IF NOT EXISTS public."2024-2025"
 (
     major character varying(400) COLLATE pg_catalog."default" NOT NULL,
     majoryear character varying(20) COLLATE pg_catalog."default" NOT NULL,
     title character varying(255) COLLATE pg_catalog."default" NOT NULL,
     prereq character varying(800) COLLATE pg_catalog."default",
-    CONSTRAINT courses_pkey PRIMARY KEY (major, majoryear, title)
+    coreq character varying(800) COLLATE pg_catalog."default",
+    unit character varying(50) COLLATE pg_catalog."default",
+    CONSTRAINT updatedcourses_pkey PRIMARY KEY (major, majoryear, title)
 );`;
 
 export default makeCourseTable; 
